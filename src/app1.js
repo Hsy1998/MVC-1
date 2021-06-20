3026f24726d477f4ab6e6bacf1f7df99edae6bf6
Hsy <a553331998@gmail.com>
2021-06-20 23:45:37 +0800

    MVC优化

--- src/app1.js
+++ src/app1.js
@@ -1,19 +1,25 @@
 import './app1.css'
 import $ from 'jquery'
 
+const eventBus = $(window)
+
 // 数据相关放m
 const m = {
   data: {
     // 初始化数据
     n: parseInt(localStorage.getItem('n'))
-  }
+  },
+  update(data) {
+    Object.assign(m.data, data)
+    eventBus.trigger('m:updated')
+    localStorage.setItem('n', m.data.n)
+  },
 }
 
 // 视图相关放到v
 const v = {
   el: null,
   html: `
-  <div id="app1">
       <div id="number">{{n}}</div>
       <div>
         <button id="btnAdd">+1</button>
@@ -21,71 +27,63 @@ const v = {
         <button id="btnMultiply">*2</button>
         <button id="btnDivide">÷2</button>
       </div>
-  </div>
 `,
   init(container) {
-    v.container = $(container)
-    v.render()
+    v.el = $(container)
   },
-  render() {
-    if (v.el === null) {
-      v.el = $(v.html.replace('{{n}}', m.data.n)).prependTo(v.container)
-    } else {
-      const newEl = $(v.html.replace('{{n}}', m.data.n))
-      v.el.replaceWith(newEl)
-      v.el = newEl
-    }
-
+  render(n) {
+    if (v.el.children.length !== 0) v.el.empty() // 清空el里的内容再赋值
+    $(v.html.replace('{{n}}', n)).appendTo(v.el)
   },
 }
 
-
-// 初始化html
-const html = `
-  <div id="app1">
-      <div id="number">100</div>
-      <div>
-        <button id="btnAdd">+1</button>
-        <button id="btnMinus">-1</button>
-        <button id="btnMultiply">*2</button>
-        <button id="btnDivide">÷2</button>
-      </div>
-  </div>
-`
 // 其他操作都放c
 const c = {
   init(container) {
     // 第一次渲染html
     v.init(container)
-    c.ui = {
-      btnAdd: $('#btnAdd'),
-      btnMinus: $('#btnMinus'),
-      btnMultiply: $('#btnMultiply'),
-      btnDivide: $('#btnDivide'),
-      number: $('#number')
-    }
-    c.bindEvents()
+    c.autoBindEvents()
+    v.render(m.data.n)
+    eventBus.on('m:updated', () => {
+      v.render(m.data.n)
+    })
   },
-  // 获取重要的元素
 
-  bindEvents() {
-    // 绑定鼠标事件
-    v.container.on('click', '#btnAdd', () => {
-      m.data.n += 1
-      v.render()
+  events: {
+    'click #btnAdd': 'add',
+    'click #btnMinus': 'minus',
+    'click #btnMultiply': 'mul',
+    'click #btnDivide': 'div'
+  },
+  add() {
+    m.update({
+      n: m.data.n + 1
     })
-    v.container.on('click', '#btnMinus', () => {
-      m.data.n -= 1
-      v.render()
+  },
+  minus() {
+    m.update({
+      n: m.data.n - 1
     })
-    v.container.on('click', '#btnMultiply', () => {
-      m.data.n *= 2
-      v.render()
+  },
+  mul() {
+    m.update({
+      n: m.data.n * 2
     })
-    v.container.on('click', '#btnDivide', () => {
-      m.data.n /= 2
-      v.render()
+  },
+  div() {
+    m.update({
+      n: m.data.n / 2
     })
+  },
+
+  autoBindEvents() {
+    for (let key in c.events) {
+      const value = c[c.events[key]]
+      const spaceIndex = key.indexOf(' ')
+      const part1 = key.slice(0, spaceIndex)
+      const part2 = key.slice(spaceIndex + 1)
+      v.el.on(part1, part2, value)
+    }
   }
 
 }
--- src/app2.js
+++ src/app2.js
@@ -1,31 +1,76 @@
 import './app2.css'
 import $ from 'jquery'
 
-const html = `
-<div id="app2">
-      <ol id="tab-bar">
-        <li>1</li>
-        <li>2</li>
-      </ol>
-      <ol id="content">
-        <li>1</li>
-        <li>2</li>
-      </ol>
-    </div>
+const eventBus = $(window)
+
+const m = {
+  data: {
+    // 初始化数据
+    index: parseInt(localStorage.getItem('index'))
+  },
+  update(data) {
+    Object.assign(m.data, data)
+    eventBus.trigger('m:updated')
+    localStorage.setItem('index', m.data.index)
+  },
+}
+
+// 视图相关放到v
+const v = {
+  el: null,
+  html: (index) => {
+    return `
+   <ol id="tab-bar">
+     <li class="${index === 0 ? 'active' : null}" data-index="0">1</li>
+     <li class="${index === 1 ? 'active' : null}" data-index="1">2</li>
+   </ol>
+   <ol id="content">
+     <li class="${index === 0 ? 'block' : null}">1</li>
+     <li class="${index === 1 ? 'block' : null}">2</li>
+   </ol>
 `
+  },
+  init(container) {
+    v.el = $(container)
+  },
+  render(index) {
+    if (v.el.children.length !== 0) v.el.empty()
+    $(v.html(index)).appendTo(v.el)
+  },
+}
 
-const $element = $(html).appendTo($('body>#page'))
+// 其他操作都放c
+const c = {
+  init(container) {
+    // 第一次渲染html
+    v.init(container)
+    v.render(m.data.index)
+    c.autoBindEvents()
+    eventBus.on('m:updated', () => {
+      v.render(m.data.index)
+    })
+  },
+  events: {
+    'click #tab-bar li': 'index',
+  },
+  index(e) {
+    const index = parseInt(e.currentTarget.dataset.index);
+    console.log(index);
+    m.update({
+      index: index
+    })
+  },
 
-const $tabBar = $('#tab-bar')
-const $content = $('#content')
-const n = localStorage.getItem('index')
+  autoBindEvents() {
+    for (let key in c.events) {
+      const value = c[c.events[key]]
+      const spaceIndex = key.indexOf(' ')
+      const part1 = key.slice(0, spaceIndex)
+      const part2 = key.slice(spaceIndex + 1)
+      v.el.on(part1, part2, value)
+    }
+  }
+}
 
-$tabBar.on('click', 'li', (e) => {
-  const $li = $(e.currentTarget);
-  $li.addClass('active').siblings().removeClass('active')
-  const index = $li.index()
-  const n = localStorage.setItem('index', index)
-  $content.children().eq(index).addClass('block').siblings().removeClass('block')
-})
 
-$tabBar.children().eq(n).trigger('click')
+export default c
